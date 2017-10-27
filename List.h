5d6ab26d8155bbc110c4f94fb058a9d69a9a9d83
realkod246 <realkod246@users.noreply.github.com>
2017-10-27 18:33:14 -0400

    List only

--- List.h
+++ List.h
@@ -14,10 +14,12 @@ class List //begin List definition
     int num_elements = 0;
     
   public:
+	  List()
      ~List();//destructor
      void insert(int element, int k);//insert element at location k
      void remove(int k);//remove element at location k
      int size();//return the number of elements in the List
+     void get (int k);
      
      
      /** MISSING OPERATIONS */
